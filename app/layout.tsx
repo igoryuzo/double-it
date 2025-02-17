5bc9d67560bdcd23040659ad4d4931d94e5f7bf6
Igor Yuzovitskiy <igor.yuzovitskiy@gmail.com>
2025-02-16 19:29:35 -0500

    meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,50 +13,50 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const appUrl = process.env.NEXT_PUBLIC_URL;
+// const appUrl = process.env.NEXT_PUBLIC_URL;
 
-const frame = {
-  version: "vNext",
-  imageUrl: `${appUrl}/images/feed.png`,
-  button: {
-    title: "Start",
-    action: {
-      type: "launch_frame",
-      name: "Double It",
-      url: appUrl,
-      splashImageUrl: `${appUrl}/images/splash.png`,
-      splashBackgroundColor: "#f7f7f7",
-    },
-  },
-};
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Double It",
-    openGraph: {
-      title: "Double It",
-      description: "Double it or withdraw it. Form the longest chain.",
-    },
-    other: {
-      "fc:frame": JSON.stringify(frame),
-    },
-  };
-}
-
-// export const metadata: Metadata = {
-//   title: "Double It",
-//   description: "Double It",
-//   openGraph: {
-//     title: "Double It",
-//     description: "Double it or withdraw it. Form the longest chain.",
+// const frame = {
+//   version: "vNext",
+//   imageUrl: `${appUrl}/images/feed.png`,
+//   button: {
+//     title: "Start",
+//     action: {
+//       type: "launch_frame",
+//       name: "Double It",
+//       url: appUrl,
+//       splashImageUrl: `${appUrl}/images/splash.png`,
+//       splashBackgroundColor: "#f7f7f7",
+//     },
 //   },
-//   other: {
-//     'fc:frame:image': `${process.env.NEXT_PUBLIC_URL}/images/feed.png`,
-//     'fc:frame:button:1': 'Double It!',
-//     'fc:frame:post_url': `${process.env.NEXT_PUBLIC_URL}/api/frame`,
-//     'fc:frame:version': 'vNext',
-//   }
 // };
+
+// export async function generateMetadata(): Promise<Metadata> {
+//   return {
+//     title: "Double It",
+//     openGraph: {
+//       title: "Double It",
+//       description: "Double it or withdraw it. Form the longest chain.",
+//     },
+//     other: {
+//       "fc:frame": JSON.stringify(frame),
+//     },
+//   };
+// }
+
+export const metadata: Metadata = {
+  title: "Double It",
+  description: "Double It",
+  openGraph: {
+    title: "Double It",
+    description: "Double it or withdraw it. Form the longest chain.",
+  },
+  other: {
+    'fc:frame:image': `${process.env.NEXT_PUBLIC_URL}/images/feed.png`,
+    'fc:frame:button:1': 'Double It!',
+    'fc:frame:post_url': `${process.env.NEXT_PUBLIC_URL}/api/frame`,
+    'fc:frame:version': 'vNext',
+  }
+};
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,35 @@
-// import { Metadata } from "next";
+import { Metadata } from "next";
 import App from "./app";
 
-// const appUrl = process.env.NEXT_PUBLIC_URL;
+const appUrl = process.env.NEXT_PUBLIC_URL;
 
-// const frame = {
-//   version: "next",
-//   imageUrl: `${appUrl}/images/feed.png`,
-//   button: {
-//     title: "Press Me",
-//     action: {
-//       type: "launch_frame",
-//       name: "Frame V2 Starter",
-//       url: appUrl,
-//       splashImageUrl: `${appUrl}/images/splash.png`,
-//       splashBackgroundColor: "#f7f7f7",
-//     },
-//   },
-// };
+const frame = {
+  version: "next",
+  imageUrl: `${appUrl}/images/feed.png`,
+  button: {
+    title: "Press Me",
+    action: {
+      type: "launch_frame",
+      name: "Frame V2 Starter",
+      url: appUrl,
+      splashImageUrl: `${appUrl}/images/splash.png`,
+      splashBackgroundColor: "#f7f7f7",
+    },
+  },
+};
 
-// export async function generateMetadata(): Promise<Metadata> {
-//   return {
-//     title: "Frame V2 Starter",
-//     openGraph: {
-//       title: "Frame V2 Starter",
-//       description: "A starter for Frame v2",
-//     },
-//     other: {
-//       "fc:frame": JSON.stringify(frame),
-//     },
-//   };
-// }
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Double It",
+    openGraph: {
+      title: "Double It",
+      description: "Double it or withdraw it. Form the longest chain.",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
+}
 
 export default function Home() {
   return <App />;
